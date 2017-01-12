20911ba6cc9bf3e87cb2eab056a358324ce50fad
pfeiffer <tim.pfeiffer002@stud.fh-dortmund.de>
2017-01-12 11:00:11 +0100

    project init

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by pfeiffer on 12.01.2017.
+ */
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
